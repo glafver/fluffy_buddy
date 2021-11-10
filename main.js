d94313df3ecd7401b819c3bc3d3352c5cb27cb3b
glafver <glafver22@gmail.com>
2021-11-10 16:51:57 +0100

    added fluffy_buddy poject

---
+++ main.js
@@ -0,0 +1,23 @@
+function addedToCart() {
+  alert("The item was added to your cart!");
+}
+
+function woof1() {
+  var audio = new Audio("content/woof1.mp3");
+  audio.play();
+}
+
+function woof2() {
+  var audio = new Audio("content/woof2.mp3");
+  audio.play();
+}
+
+function woof3() {
+  var audio = new Audio("content/woof3.mp3");
+  audio.play();
+}
+
+function woof4() {
+  var audio = new Audio("content/woof4.mp3");
+  audio.play();
+}
